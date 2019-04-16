e8aacc9e287db3829cf50bf0188dc07a3169f057
Hilding Wollbo <Hilding@hildings-air.mynet>
2019-04-16 18:56:07 +0200

    running averages

--- HMMexercise/obs2index.m
+++
@@ -1,7 +0,0 @@
-function index = obs2index(observation, key, ind)
-%requires 1-to-1 key, ind
-index = zeros(length(observation),1);
-for i = 1:length(ind)
-    index(observation == key(i)) = ind(i);
-end
-end
