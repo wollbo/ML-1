38719ef0dc6edc8b5e4a186b045fa72077f3c471
Hilding Wollbo <wollbo@kth.se>
2019-05-15 17:15:51 +0200

    main

--- Demo/main.m
+++ Demo/main.m
@@ -1,23 +1,30 @@
-addpath D:/MATLAB/Repos/ML-1/PattRecClasses
-addpath melodies
-addpath GetMusicFeatures
+% addpath D:/MATLAB/Repos/ML-1/PattRecClasses
+% addpath melodies
+% addpath GetMusicFeatures
 
-% feed working dir to classRead
 
 winsize = (2*10^(-2));
 fs = 44100;
+A = 440;
+
 
 % update 'workingDir' dynamically
 audio = classRead('Demo','uti'); % maybe replace with 'workingDir, 'list of music dirs'
 
-
+for i = 1:size(audio,2)
+    matr = GetMusicFeatures(audio(:,i),fs,winsize);
+    fMat(1:size(matr,1),1:size(matr,2),i) = matr; 
+    feature = featureExtractDiscrete(fMat(:,:,i),A); % continuous sometimes produces imaginary numbers?
+    features(1:size(feature,1),1:size(feature,2),i) = feature;
+end
 
 %%
 
-gaussian = GaussD('Mean', 0, 'StDev', 1);
 
+gaussian = GaussD('Mean', A, 'StDev', 10);
 nStates = 16;
 pD = gaussian;
-obsData = a1;
 
-hmm=MakeLeftRightHMM(nStates,pD,features1);
+features
+
+hmm=MakeLeftRightHMM(nStates,pD,features);
